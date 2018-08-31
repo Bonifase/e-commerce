2c99bc4542b2f7d28ef6626bf5d298327e0f9b22
Bonifase Orwa <orwabonifase@gmail.com>
2018-08-31 23:45:45 +0300

    add mailing logic

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+
+
 from .forms import contactForm
+
 
 # Create your views here.
 
@@ -7,7 +12,13 @@ from .forms import contactForm
 def contact(request):
     form = contactForm(request.POST or None)
     if form.is_valid():
-        print (request.POST)
+        name = form.cleaned_data['name']
+        comment = form.cleaned_data['comment']
+        subject = 'Message from me'
+        message = '%s %s' %(name, comment)
+        emailFrom = form.cleaned_data['email']
+        emailTo = [settings.EMAIL_HOST_USER]
+        send_mail(subject, message, emailFrom, emailTo, fail_silently=False,)
     context = locals()
     template = 'contact.html'
     return render(request, template, context)
