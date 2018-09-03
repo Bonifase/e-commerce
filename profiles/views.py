d24b3ffc539de97a6672c9c9c923ffdfa2dff2b1
Bonifase Orwa <orwabonifase@gmail.com>
2018-09-03 17:09:59 +0300

    add user profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -15,4 +16,9 @@ def about(request):
     return render(request, template, context)
 
 
-
+@login_required
+def userProfile(request):
+    user = request.user
+    context = {'user': user}
+    template = 'profile.html'
+    return render(request, template, context)
