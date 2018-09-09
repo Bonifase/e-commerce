6a75efd345bd10744a4c7fdfb03cc4d4839ef938
Bonifase Orwa <orwabonifase@gmail.com>
2018-09-09 18:44:46 +0300

    configure heroku deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 
-STATICFILES_STORAGE = ‘whitenoise.storage.CompressedManifestStaticFilesStorage’
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 if DEBUG:
